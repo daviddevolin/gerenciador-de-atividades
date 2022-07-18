3de34bfe296711267c849dfbb727812b07d7cfa8
daviddevolin <davidccb@live.com>
2022-07-18 18:31:13 -0300

    criando component singin

---
+++ SIG-Tasks/src/screens/SingIn.tsx
@@ -0,0 +1,12 @@
+import { View , Text } from 'react-native';
+export function SingIn (){
+    return(
+        <View>
+            <text>
+                
+            </text>
+        </View>
+    )
+        
+    
+}
