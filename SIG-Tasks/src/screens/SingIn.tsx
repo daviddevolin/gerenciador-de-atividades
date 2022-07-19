3e2e9c3640f288847e7fa9f4f69c8858ac131b77
daviddevolin <davidccb@live.com>
2022-07-19 00:38:27 -0300

    criando component Button

--- SIG-Tasks/src/screens/SingIn.tsx
+++ SIG-Tasks/src/screens/SingIn.tsx
@@ -1,7 +1,13 @@
-import { VStack, Heading } from 'native-base';
+import { VStack, Heading , Icon, useTheme} from 'native-base';
 import Logo from '../assets/logo_primary.svg';
+import { Input } from '../components/Input';
+import { Envelope , Key} from 'phosphor-react-native';
+import { Button } from '../components/Button';
 
 export function SingIn (){
+
+    const { colors } = useTheme() ;
+
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24} >
             <Logo />
@@ -10,6 +16,19 @@ export function SingIn (){
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
             </Heading>
+
+            <Input placeholder = "E-mail"
+                mb={4}
+                InputLeftElement ={<Icon as= {<Envelope color= {colors.gray[300]} />} ml={4}/>} 
+            />
+            <Input placeholder = "Senha" 
+                mb={8}
+                InputLeftElement ={<Icon as= {<Key  color= {colors.gray[300]} />} ml={4} />} 
+                secureTextEntry
+            />
+
+            <Button title="Entrar"  w="full" />
+
         </VStack>
     );
         
