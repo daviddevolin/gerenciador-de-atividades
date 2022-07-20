0c00627197a9436cd87811abb5dbba42f64cdc7c
daviddevolin <davidccb@live.com>
2022-07-19 23:49:01 -0300

    crinado routes

---
+++ SIG-Tasks/src/routes/app.routes.tsx
@@ -0,0 +1,4 @@
+import { Home } from "../screens/Home";
+import { Register } from "../screens/Register";
+import { SingIn } from "../screens/SingIn";
+import { Dtails } from "../screens/Dtails";
---
+++ SIG-Tasks/src/screens/Dtails.tsx
@@ -0,0 +1,10 @@
+import { VStack } from 'native-base';
+import { Header } from '../components/Header';
+
+export function Dtails() {
+  return (
+    <VStack flex={1} bg="gray.700">
+        <Header title="solicitação"/>
+    </VStack>
+  );
+}
